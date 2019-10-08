5513482c39dc678f14568de14a45c9b785cadbb8
Weygers Senne <senne.weygers@immalle.eu>
2019-10-08 11:45:07 +0200

    Challenge 5 complete

--- 5 - Numeric variables/code_challenge.py
+++ 5 - Numeric variables/code_challenge.py
@@ -1,6 +1,14 @@
 # Ask a user to enter a number
+first_number = input('Geef een nummer in: ')
+
 # Ask a user to enter a second number
+second_number = input('Geef een tweede nummer in: ')
+
 # Calculate the total of the two numbers added together
-# Print 'first number + second number = answer' 
+total = float(first_1number) + float(second_number)
+
+# Print 'first number + second number = answer'
+print(first_number + ' + ' + second_number + ' = ' + str(total))
+
 # For example if someone enters 4 and 6 the output should read
 # 4 + 6 = 10
